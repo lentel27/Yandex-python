7ab70a242a6882550aa82703527befe0224bd2fd
lentel27 <57633020+lentel27@users.noreply.github.com>
2019-11-13 18:35:06 +0500

    Git и случайные окружности

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
-from PyQt5.QtGui import QPen, QPainter
+from PyQt5.QtGui import QPen, QPainter, QColor
 from PyQt5.QtCore import Qt
 from PyQt5 import uic
+from UI import Ui_MainWindow
 import random
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("UI.ui", self)
+        self.setupUi(self)
         self.draw = False
         self.circle_add.clicked.connect(self.trueDraw)
 
@@ -25,7 +26,9 @@ class MyWidget(QMainWindow):
         qp.setBrush(Qt.black)
         qp.drawRect(0, 0, self.width(), self.height())
         if self.draw:
-            qp.setPen(Qt.yellow)
+            qp.setPen(QColor(random.randrange(0, 256),
+                             random.randrange(0, 256),
+                             random.randrange(0, 256)))
             size = random.randrange(100, 500)
             qp.drawEllipse(random.randrange(-100, self.width()),
                            random.randrange(-100, self.height()),
